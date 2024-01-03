b4bf4418aa126b8f29daee091a73ba945e0b3dac
Thaís Amancio <thaisamancio1300@gmail.com>
2024-01-02 22:28:40 -0300

    Desafio Finalizado!

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,19 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia: Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+         public Nokia(string numero, string modelo, string imei, int memoria) 
+                    : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando um aplicativo nesse aparelho");
+            Thread.Sleep(1000);
+            Console.WriteLine($"...");
+            Thread.Sleep(1000);
+            Console.WriteLine($"# {nomeApp} - instalado com sucesso");        
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("iPhone:");
+
+Smartphone iphone = new Iphone(numero: "78695", modelo: "iphone12", imei: "10101010", memoria: 128);
+
+iphone.LigarAparelho();
+iphone.InstalarAplicativo("Instagram");
+iphone.InstalarAplicativo("WhatsApp");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Teams");
+iphone.DesligarAparelho();
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Nokia:");
+
+Smartphone nokia = new Nokia(numero: "4545262", modelo: "tijoloQuebraTesta", imei: "2222222", memoria: 64);
+nokia.LigarAparelho();
+nokia.Ligar("Thais");
+nokia.InstalarAplicativo("Instagram");
+nokia.InstalarAplicativo("Discord");
+iphone.ReceberLigacao();
+nokia.InstalarAplicativo("Destiny Companion");
+nokia.DesligarAparelho();
